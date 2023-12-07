f9168ac02ec7a926752554e7a7c89a3c230f455a
hmun1 <hmun86074@gmil.com>
2023-12-07 22:15:36 +0600

    Beecrowd problem 1534 solved

---
+++ beecrowd_1534.py
@@ -0,0 +1,27 @@
+while True:
+    try:
+        flag = 0
+        n = int(input())
+        if n>=3 and n<70:
+            x = n-1
+            for row in range(0, n):
+                try:
+                    for column in range(0, n):
+                            if column == x:
+                                print('2', end = '')
+                            elif column == row:
+                                print('1', end = '')
+                            else:
+                                print('3', end = '')
+                    print('\n')
+                    x -= 1
+
+                except IndexError:
+                    pass
+        elif n == 0:
+            flag = 1 
+    except EOFError:   
+        break
+    if flag == 1:
+            break
+                                          
