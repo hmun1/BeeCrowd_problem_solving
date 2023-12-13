4a88155352a3ab38d096a943bee234932790babc
hmun1 <hmun86074@gmil.com>
2023-12-13 20:20:25 +0600

    Beecrowd Problem 2581 solved

---
+++ beecrowd_2581.py
@@ -0,0 +1,9 @@
+def answer(test_cases):
+    i = 0
+    while i != test_cases:
+        question = input().split(' ')
+        print('I am Toorg!')
+        i += 1
+
+test_cases = int(input())
+answer(test_cases)
