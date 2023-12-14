ec5eb195ba373d817c973118b29167d35e23da21
hmun1 <hmun86074@gmil.com>
2023-12-15 00:23:26 +0600

    Beecrowd Problem 2747 solved with python

---
+++ beecrowd_2747.py
@@ -0,0 +1,14 @@
+for i in range(0, 39):
+    print('-', end = '')
+print()
+
+for i in range(0, 5):
+    print('|',end = "")
+    for j in range(0, 37):
+        print(' ', end = '')
+    print('|')
+
+for i in range(0, 39):
+    print('-', end = '')
+print()
+
